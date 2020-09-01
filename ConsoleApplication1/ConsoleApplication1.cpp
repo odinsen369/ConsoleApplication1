117d15ebac43b64baf8e65f43e9ef7d5d312026f
odinsen369 <70570229+odinsen369@users.noreply.github.com>
2020-09-01 12:01:06 +0100

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+int age;
+
+int main () { 
+	std::cout << "enter your age :";
+	std::cin >> age;
+	std::cout << "\t your age is : " << age << "\n"; // \n = std:endl; \t=tab
+	std::cout << "you are young ";
+
+
+}
